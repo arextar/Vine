a585734a42ba782cbdce5e76afc02cbb69b1c1b0
Alex Wilson <arexkun11@gmail.com>
2011-06-03 20:12:56 -0700

    Edited vine.js via GitHub

--- vine.js
+++ vine.js
@@ -104,7 +104,7 @@ if(arr.length>1) for(x in arr) vine.bind(obj,arr[x],fn)
             Event: function (e) {
                 for (var x in e) this[x] = this[x] || e[x];
 
-                this[dp] = tpeof e.returnValue=="undefined"?e[dp]:!e.returnValue;
+                this[dp] = typeof e.returnValue=="undefined"?e[dp]:!e.returnValue;
 
                 this.timestamp = new Date().getTime();
                 this.event=e;
