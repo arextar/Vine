54d695786ad2a70106454228169995758c13a553
arexkun <arexkun11@gmail.com>
2011-07-07 13:12:15 -0400

    worked on delegate

--- plugins/delegate.js
+++ plugins/delegate.js
@@ -1,7 +1,27 @@
+vine._c={};
+
+vine.re=function(str){
+  return this._c[str]=this._c[str]||new RegExp(str);
+}
+
 vine.is=document.createElement("a").webkitMatchesSelector?function(elem,sel){
   return elem.webkitMatchesSelector(sel)
-}:function(elem,d){
-    return elem.nodeName.toLowerCase()==d;
+}:function(elem,sel,tag,id,x,l,c){
+  
+  sel=sel.split(".");
+  tag=/^(\w+)/.exec(sel[0])
+  id=/#(\w+)/.exec(sel[0]);
+  
+  c=elem.className;
+  
+  sel.shift()
+  l=sel.length;
+  for(x=0;x<l;x++){
+    if(!vine.re("(^|\\s)"+l[x]+"(\\s|$)").test(c)) return false; 
+  }
+  
+    return !tag||elem.nodeName.toLowerCase()==tag[0]
+    &&     !id||elem.id==id[1];
 }
 
 vine.delegate=function(context,selector,type,handler,data){
@@ -20,7 +40,7 @@ vine.delegate=function(context,selector,type,handler,data){
         dat.a[type]=1;
     
         vine.bind(context,type,function(e,d,l){
-            var delegated=vine.d(context).d
+            var delegated=vine.d(context).d[e.type]
             l=delegated.length;
             while(l--){
                 d=delegated[l];
@@ -41,6 +61,6 @@ vine.undelegate=function(context,selector,type){
     for(x=0;x<list.length;x++){
         if(list[x].s!=selector) ret.push(list[x]);
     }
-    alert(ret)
+    
     dat.d[type]=ret;
 }
